7b2b0332404c05069471a2cde5133e4c5f2062da
Grace Shih <vocalise@gmail.com>
2018-05-07 09:02:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,3 +34,6 @@ puts find_the_cheese(snacks)
  
 soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 puts find_the_cheese(soup)
+
+ingredients = ["garlic", "rosemary", "bread"]
+find_the_cheese(ingredients)
